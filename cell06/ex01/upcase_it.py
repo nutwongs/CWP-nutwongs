5716920efa49d8b39cb7c11c9849935723a9afe8
nutwongs <68050151@kmitl.ac.th>
2026-09-23 14:10:50 +0700

    add ex01 and ex02

--- cell06/ex01/upcase_it.py
+++ cell06/ex01/upcase_it.py
@@ -3,4 +3,4 @@
 def upcase_it(text):
     return text.upper()
 
-print(upcase_it("hello"))
+print(upcase_it("hello hi"))
---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+def downcase_it(text):
+    return text.lower()
+
+params = sys.argv[1:]
+
+if not params:
+    print("none")
+else:
+    for param in params:
+        print(downcase_it(param))
