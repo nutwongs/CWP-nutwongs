94a0cc0b44ef02a4dd07c9bd4ca1065952d9563c
nutwongs <68050151@kmitl.ac.th>
2026-09-23 11:36:01 +0700

    add ex09 and ex10

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+if len(sys.argv) != 3:
+    print("none")
+else:
+    keyword = sys.argv[1]
+    text = sys.argv[2]
+    matches = re.findall(keyword, text)
+    
+    if not matches:
+        print("none")
+    else:
+        print(len(matches))
