3264c6d8b8c9826088b5da8368c50638d802ffca
nutwongs <68050151@kmitl.ac.th>
2026-09-23 13:29:32 +0700

    add ex13 and ex14

---
+++ cell05/ex13/append_it.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+params = sys.argv[1:]
+
+if not params:
+    print("none")
+else:
+    for param in params:
+        if not param.endswith("ism"):
+            print(f"{param}ism")
+            
---
+++ cell05/ex14/free_range.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) != 3:
+    print("none")
+else:
+    try:
+        start = int(sys.argv[1])
+        end = int(sys.argv[2])
+
+        if start <= end:
+            result = list(range(start, end + 1))
+        else:
+            result = list(range(start, end - 1, -1))
+
+        print(result)
+    except ValueError:
+        print("none")
